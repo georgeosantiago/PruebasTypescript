503e0988963ce91cf85d74a635fd6915ef792a27
Jorge Santiago <georgeosantiago@users.noreply.github.com>
2018-09-09 12:15:56 -0300

    VariableDeclarations

--- VariableDeclarations.ts
+++ VariableDeclarations.ts
@@ -94,3 +94,91 @@ let { a, b } = o;
 console.log('##########'); 
 console.log(a); 
 console.log(b); 
+
+({ a, b } = { a: "baz", b: 101 });
+console.log(a); 
+console.log(b); 
+
+let { a:aa, ...passthrough } = o;
+let total = passthrough.b + passthrough.c.length;
+console.log('##########'); 
+console.log(aa); 
+console.log(total);
+
+// Property renaming
+
+let { a: newName1, b: newName2 } = o;
+console.log('##########'); 
+console.log(newName1); 
+console.log(newName2);
+
+let { a:a_string, b:b_number }: { a: string, b: number } = o;
+console.log('##########'); 
+console.log(a_string); 
+console.log(b_number);
+
+// Default values
+
+function keepWholeObject(wholeObject: { a: string, b?: number }) {
+    let { a, b = 1001 } = wholeObject;
+    console.log(a); 
+    console.log(b); 
+}
+console.log('##########'); 
+keepWholeObject({a:'hola', b:10});
+keepWholeObject({a:'hola'});
+
+// Function declarations
+
+type C = { a: string, b?: number }
+function mifun({ a, b=99 }: C): void {
+    console.log(a); 
+    console.log(b);     
+}
+console.log('##########'); 
+mifun({a:'changos', b:11});
+mifun({a:'changos'});
+
+function ofun({ a="", b=0 } = {}): void {
+    console.log(a); 
+    console.log(b);     
+}
+console.log('##########'); 
+ofun();
+ofun({a:'yy',b:4});
+
+function xfun({ a, b = 0 } = { a: "--" }): void {
+    console.log(a); 
+    console.log(b);         
+}
+console.log('##########'); 
+xfun({ a: "yes" }); // ok, default b = 0
+xfun(); // ok, default to { a: "--" }, which then defaults b = 0
+// xfun({}); // error, 'a' is required if you supply an argument
+
+// Spread
+
+let afirst = [1, 2];
+let asecond = [3, 4];
+let bothPlus = [0, ...afirst, ...asecond, 5];
+console.log(bothPlus); 
+
+let defaults = { food: "spicy", price: "$$", ambiance: "noisy" };
+let search = { ...defaults, food: "rich" };
+console.log(search); //cambio el valor de food, a default le aplicamos food
+
+let search_b = { food: "rich", ...defaults };
+console.log(search_b); //a food le aplicamos default
+
+
+// La propagación de objetos solo incluye las propiedades enumerables
+// y no permite extensiones de parámetros de tipo a partir de funciones genéricas
+class Cla {
+    p = 12;
+    m() {
+    }
+  }
+  let vc = new Cla();
+  let clonado = { ...vc };
+  clonado.p; // ok
+  // clonado.m(); // error!
