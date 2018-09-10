8c7cff0d95b84c688ad8423c1ee60116fbc96383
Jorge Santiago <jsantiago@newcom-lcs.com>
2018-09-10 15:25:42 -0300

    ok

--- Functions.ts
+++ Functions.ts
@@ -74,3 +74,100 @@ let employeeName = buildName_rest("Joseph", "Samuel", "Lucas", "MacKinzie");
 let buildNameFun: (fname: string, ...rest: string[]) => string = buildName_rest;
 
 /// this
+
+let deck = {
+    suits: ["hearts", "spades", "clubs", "diamonds"],
+    cards: Array(52),
+    createCardPicker: function() {
+        // return function() { Error: this es undefined
+        // Las funciones de flecha (ECMAScript 6) capturan this dónde se crea la función en lugar de dónde se invoca
+        return () => {
+            let pickedCard = Math.floor(Math.random() * 52);
+            let pickedSuit = Math.floor(pickedCard / 13);
+
+            return {suit: this.suits[pickedSuit], card: pickedCard % 13};
+        }
+    }
+}
+
+let cardPicker = deck.createCardPicker();
+let pickedCard = cardPicker();
+
+console.log('######');
+console.log("card: " + pickedCard.card + " of " + pickedCard.suit);
+
+// this parameters
+
+function fun_a(this: void) {
+    // make sure `this` is unusable in this standalone function
+}
+
+// this con interface para definir el tipo
+interface Card {
+    suit: string;
+    card: number;
+}
+interface Deck {
+    suits: string[];
+    cards: number[];
+    createCardPicker(this: Deck): () => Card;
+}
+let odeck: Deck = {
+    suits: ["hearts", "spades", "clubs", "diamonds"],
+    cards: Array(52),
+    // NOTE: The function now explicitly specifies that its callee must be of type Deck
+    createCardPicker: function(this: Deck) {
+        return () => {
+            let pickedCard = Math.floor(Math.random() * 52);
+            let pickedSuit = Math.floor(pickedCard / 13);
+
+            return {suit: this.suits[pickedSuit], card: pickedCard % 13};
+        }
+    }
+}
+
+let vcardPicker = odeck.createCardPicker();
+let vpickedCard = vcardPicker();
+
+console.log('######');
+console.log("card: " + vpickedCard.card + " of " + vpickedCard.suit);
+
+// this parameters in callbacks
+
+interface UIElement {
+    addClickListener(onclick: (this: void, e: Event) => void): void;
+}
+
+
+class Handler {
+    info: string;
+    onClickGood(this: void, e: Event) {
+        // can't use this here because it's of type void!
+        console.log('clicked!');
+    }
+}
+let h = new Handler();
+//uiElement.addClickListener(h.onClickGood);  Ok
+
+// usar funcio flecha
+class Handler2 {
+    info: string;
+    onClickGood = (e: Event) => { this.info = e.type }
+}
+
+// Overloads
+
+function  funalgo(x: {v1: string, v2:number; }): number;
+function  funalgo(x: number): {v1: string, v2: number; };
+function  funalgo(x: any): any {
+    if (typeof x == "object") {
+        return 10;
+    }
+    else if (typeof x == "number") {
+        return {v1:'hola', v2: 5};
+    }
+}
+console.log('######');
+console.log( funalgo({v1:'hola', v2: 5}) );
+console.log( funalgo(5) );
+
